1fdf2fff6ff4a6c1d5e60c1add20d9360cd8870c
vrangelorient <79807806+vrangelorient@users.noreply.github.com>
2021-07-03 13:23:06 +0500

    Add files via upload

---
+++ LinearSVC_persian_dari.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import os
+import json
+import re
+from pprint import pprint
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import random; random.seed(44)
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn import metrics
+
+
+# In[2]:
+
+
+train = pd.read_json("D:/Uchyoba_gumanitaristika/diplom/SVM/news_subsample.json")
+X_train, X_test, y_train, y_test = train_test_split(train["text"], train["language"], random_state=44, test_size=0.3)
+
+
+# In[3]:
+
+
+tfidf = TfidfVectorizer(min_df=0.05, max_df=0.9)
+tfidf_train = tfidf.fit_transform(X_train)
+tfidf_test = tfidf.transform(X_test)
+
+
+# In[4]:
+
+
+svc = LinearSVC()
+svc.fit(tfidf_train, y_train)
+svc_pred = svc.predict(tfidf_test)
+svc_score = metrics.accuracy_score(y_test, svc_pred)
+print("LunearSVC score: ", svc_score)
+cm = metrics.plot_confusion_matrix(svc, tfidf_test, y_test,display_labels=['Farsi', 'Dari'], cmap=plt.cm.Blues)
+cm.ax_.set_title('Farsi/Dari LinearSVC Confusion Matrix')
+plt.savefig("myplot.png")
+plt.show()
+
+
+# In[20]:
+
+
+import itertools
+
+def plot_and_return_top_features(classifier, vectorizer, top_features=20):
+
+    class_labels = classifier.classes_
+    feature_names = vectorizer.get_feature_names()
+    topn_class1 = sorted(zip(classifier.coef_[0], feature_names))[:top_features]
+    topn_class2 = sorted(zip(classifier.coef_[0], feature_names))[-top_features:]
+    top_coefficients = np.hstack([topn_class1, topn_class2])
+    if set(topn_class1).union(topn_class2):
+        top_coefficients = topn_class1
+        for ce in topn_class2:
+            if ce not in topn_class1:
+                top_coefficients.append(ce)
+
+    plt.figure(figsize=(15, 5))
+    colors = ['red' if c < 0 else 'blue' for c in [tc[0] for tc in top_coefficients]]
+    plt.bar(np.arange(len(top_coefficients)), [tc[0] for tc in top_coefficients], color=colors)
+    plt.xticks(np.arange(len(top_coefficients)),
+               [tc[1] for tc in top_coefficients], rotation=60, ha='right')
+    plt.show()
+    return top_coefficients
+
+
+# In[21]:
+
+
+top_features = plot_and_return_top_features(svc, tfidf)
+
+
+# In[ ]:
+
+
+
+
